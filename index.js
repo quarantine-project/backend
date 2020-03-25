fb3eb16ab123e655a1fffa4300245a088141ab40
lily <lrcgal@gmail.com>
2020-03-25 16:59:07 -0400

    server side arch

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there'});
-});
+authRoutes(app); 
 
 // if an environmental var is establisghed by heroku use that port else local 5000
 const PORT = process.env.PORT || 5000;
